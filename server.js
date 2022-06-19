eb3b32cb80a4e59c5816001b2e9410a140e2f7bb
Madhavendra Dubey <gmind2895@gmail.com>
2022-06-19 22:37:33 +0530

    Replaced 'users array to javascript map for better optimization now when user join then user will be added to the map and when user will be disconnected then user will be automatic removed from the map object

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ const io = socket(server);
 
 const PORT = process.env.PORT||8000;
 // middlewares
+// app.use(cors({
+//     origin:" https://my-group-chat.herokuapp.com"
+// }));
+
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:" http://localhost:3000"
 }));
 
 // default api
@@ -23,29 +27,33 @@ app.get("/" , (req , res)=>{
     })
 });
 
-const users = [];
+const users = new Map();
 // using io
 io.on("connection" , (socket)=>{
     console.log("New Connection");
     console.log("loop?");
 
     socket.on("join" , ({name})=>{
-        users[socket.id] = name;
+        users.set(socket.id , name);
         // broadcasting
-        socket.broadcast.emit("user-joined" , {user:"Admin" , msg:`${users[socket.id]} has joined the chat`});  
+        socket.broadcast.emit("user-joined" , {user:"Admin" , msg:`${users.get(socket.id)} has joined the chat`});  
         // sending to the new joined user    
         socket.emit("welcome" , {user:"Admin" , msg:"Welcome to the chat"});
+
+        console.log(users);
     });
 
     // when user disconnect
     socket.on("discon" , ()=>{
             // broadcasting to all that user disconnected
-            socket.broadcast.emit("leaved-user" , {msg:`${users[socket.id]} has leaved the chat`});
+            socket.broadcast.emit("leaved-user" , {msg:`${users.get(socket.id)} has leaved the chat`});
+            users.delete(socket.id);
+            console.log(users);
     });
 
     socket.on("send-message",(data)=>{
         // broadcasting msg
-        io.emit("receive-msg" , {name:users[socket.id] , msg:data.msg , id:data.id});
+        io.emit("receive-msg" , {name:users.get(socket.id) , msg:data.msg , id:data.id});
     });
 
 });
